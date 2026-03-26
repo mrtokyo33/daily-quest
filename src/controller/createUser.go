f67492834a7ae2f1b7e84219fb7513512e988649
mrtokyo33 <mrtokyo33sec@gmail.com>
2026-03-26 19:54:11 -0300

    feat: add user request validation and error handling

--- src/controller/createUser.go
+++ src/controller/createUser.go
@@ -1,9 +1,22 @@
 package controller
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
+	"github.com/mrtokyo33/daily-quest/src/configuration/validation"
+	"github.com/mrtokyo33/daily-quest/src/model/request"
 )
 
 func CreateUser(c *gin.Context) {
+	var userRequest request.UserRequest
 
+	if err := c.ShouldBindJSON(&userRequest); err != nil {
+		restErr := validation.ValidateUserError(err)
+
+		c.JSON(restErr.Code, restErr)
+		return
+	}
+
+	fmt.Println(userRequest)
 }
